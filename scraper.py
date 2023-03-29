ef39f678ff2f95a08090e3bbeaef4d6033964750
Zuzanna Zimmer <114940033+ZuzannaZimmer@users.noreply.github.com>
2023-03-29 16:20:28 +0200

    2023-03-29 ZZ

---
+++ scraper.py
@@ -0,0 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
+#product_code = input("Podaj kod produktu: ")
+product_code = 85742588
+url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
+response = requests.get(url)
+page_dom = BeautifulSoup(response.text, "html.parser")
+print(page_dom.prettify())
