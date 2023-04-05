7a2cfa54a6418bb3960bb7d1654555a7da9b5d97
Zuzanna Zimmer <114940033+ZuzannaZimmer@users.noreply.github.com>
2023-04-05 16:35:05 +0200

    2023-04-05 ZZ

--- scraper.py
+++ scraper.py
@@ -5,4 +5,24 @@ product_code = 85742588
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response = requests.get(url)
 page_dom = BeautifulSoup(response.text, "html.parser")
-print(page_dom.prettify())
+opinions = page_dom.select("div.js_product-review")
+#print(page_dom.prettify())
+all_opinions = []
+for opinion in opinions:
+    single_opinion = {
+        "opinion_id":opinion['data-entry-id'],
+        "author":opinion.select_one("span.user-post__author-name").text.strip(),
+        "recommendation":opinion.select_one("span.user-post__author-recomendation>em").text.strip(),
+        "rating":opinion.select_one("span.user-post__score-count").text.strip(),
+        "verified":opinion.select_one("div.review-pz").text.strip(),
+        "post_date":opinion.select_one("span.user-post__published > time:nth-child(1)")["datatime"].strip(),
+        "purchase_date":opinion.select_one("span.user-post__published > time:nth-child(2)")["datatime"].strip(),
+        "vote_up":opinion.select_one("buton.vote-yes")['data-total-vote'].strip(),
+        "vote_down":opinion.select_one("buton.vote-no")['data-total-vote'].strip(),
+        "content":opinion.select_one("div.user-post__text").text.strip(),
+        "cons":[ cons.text.strip() for cons in opinion.select("div.review-feature__title—negatives~div.review-feature__item")],
+        "pros":[ pros.text.strip() for pros in opinion.select("div.review-feature__title—positives~div.review-feature__item")]
+
+    }
+    all_opinions.append(single_opinion)
+    print(type(opinion))
